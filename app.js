c79b134258b525861225f8b71f6ab4dc69ff1906
dankato <apearmor@gmail.com>
2017-10-21 00:09:04 -0700

    added accordion transition

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ for (let i = 0; i < accordion.length; i++) {
   };
 }
 
+var acc = document.getElementsByClassName('accordion');
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle('active');
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    } 
+  };
+}
+
 $(function(){
   new Clipboard('.copy-text');
 });
